d4076598ebe0911a066df8739250d2ca7f0c75a6
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-09-28 11:52:01 -0400

    thought and user routes are done. framework for controllers has been established.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,7 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+  // CRUD functions go here
+};
+
+module.exports = thoughtController;
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+const { User } = require('../models');
+
+const userController = {
+  // CRUD functions go here
+};
+
+module.exports = userController;
