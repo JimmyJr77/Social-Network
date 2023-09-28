629b75811549404dec7a72099da420fc9f16ef67
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-09-28 15:37:33 -0400

    added CRUD functionality to Controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,7 +1,58 @@
 const { Thought } = require('../models');
 
 const thoughtController = {
-  // CRUD functions go here
+  // Get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Get one thought by id
+  getSingleThought({ params }, res) {
+    Thought.findById(params.id)
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Create a thought
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Update a thought by id
+  updateThought({ params, body }, res) {
+    Thought.findByIdAndUpdate(params.id, body, { new: true, runValidators: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Delete a thought
+  deleteThought({ params }, res) {
+    Thought.findByIdAndDelete(params.id)
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
 };
 
 module.exports = thoughtController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,59 @@
 const { User } = require('../models');
 
 const userController = {
-  // CRUD functions go here
+  // Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Get one user by id
+  getSingleUser({ params }, res) {
+    User.findById(params.id)
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Create a user
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Update a user by id
+  updateUser({ params, body }, res) {
+    User.findByIdAndUpdate(params.id, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // Delete a user
+  deleteUser({ params }, res) {
+    User.findByIdAndDelete(params.id)
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
 };
 
 module.exports = userController;
+
